816caaa118be9795e6ef0d7719e67be0e386c82d
chi <heysmile1210@hotmail.com>
2016-07-20 17:03:48 +0800

    Setup firebase

--- app/src/main/java/tw/chikuo/jjcutpointcardpos/MainActivity.java
+++ app/src/main/java/tw/chikuo/jjcutpointcardpos/MainActivity.java
@@ -3,11 +3,21 @@ package tw.chikuo.jjcutpointcardpos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup Firebase library
+        Firebase.setAndroidContext(this);
+        Firebase rootRef = new Firebase("https://jjcutpointcard.firebaseio.com/");
+        Firebase userRef = new Firebase("https://jjcutpointcard.firebaseio.com/users");
+
+
+
     }
 }
---
+++ app/src/main/java/tw/chikuo/jjcutpointcardpos/User.java
@@ -0,0 +1,60 @@
+package tw.chikuo.jjcutpointcardpos;
+
+/**
+ * Created by Chi on 2016/7/20.
+ */
+public class User {
+
+    private String id ;
+    private String phone ;
+    private String gender ;
+    private int age ;
+    private String email ;
+
+    public User() {
+    }
+
+    public User(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}

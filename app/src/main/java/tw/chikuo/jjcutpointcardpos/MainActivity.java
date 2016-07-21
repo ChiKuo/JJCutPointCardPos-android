d05a175dd9703b46abf8b8c82735bb97aa0e4e66
chi <heysmile1210@hotmail.com>
2016-07-21 16:59:34 +0800

    Setup view

--- app/src/main/java/tw/chikuo/jjcutpointcardpos/MainActivity.java
+++ app/src/main/java/tw/chikuo/jjcutpointcardpos/MainActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
@@ -24,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText phoneEditText;
     private Button enterButton;
+    private LinearLayout actionLayout;
+    private LinearLayout userInfoLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +53,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
+        actionLayout = (LinearLayout) findViewById(R.id.action_layout);
+        userInfoLayout = (LinearLayout) findViewById(R.id.user_info_layout);
+
         // Phone
         phoneEditText = (EditText) findViewById(R.id.phone_edit_text);
 
-        // Enter
+        // Phone Enter
         enterButton = (Button) findViewById(R.id.enter_button);
         if (enterButton != null){
             enterButton.setOnClickListener(new View.OnClickListener() {
@@ -97,12 +105,52 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+        // Done Button
+        ImageButton doneButton = (ImageButton) findViewById(R.id.done_button);
+        if (doneButton != null) {
+            doneButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    focusUser = null;
+                    reviewUserStep();
+                }
+            });
+        }
+
+        // Exchange Button
+        ImageButton exchangeButton = (ImageButton) findViewById(R.id.exchange_button);
+        if (exchangeButton != null) {
+            exchangeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO
+
+                }
+            });
+        }
+
+        // Stamp Button
+        ImageButton stampButton = (ImageButton) findViewById(R.id.stamp_button);
+        if (stampButton != null) {
+            stampButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Add points
+
+                }
+            });
+        }
+
     }
 
     private void reviewUserStep() {
         if (focusUser != null){
+            // View control
             phoneEditText.clearFocus();
             enterButton.setVisibility(View.GONE);
+            userInfoLayout.setVisibility(View.VISIBLE);
+            userInfoLayout.setVisibility(View.VISIBLE);
 
             // Query for user's point
             Query queryRef = pointRef.orderByChild("owner").equalTo(focusUser.getId());
@@ -110,7 +158,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         } else {
+            // View control
             enterButton.setVisibility(View.VISIBLE);
+            userInfoLayout.setVisibility(View.GONE);
+            userInfoLayout.setVisibility(View.GONE);
         }
     }
 
